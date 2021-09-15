34da95cd07061999d22818b927e387dae7684305
rikupoo <nemutir@gmail.com>
2021-09-15 13:07:07 +0900

    投稿更新機能

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,4 +6,8 @@ app_name = 'myapp'
 
 urlpatterns = [
     path('', views.Index.as_view(), name = 'index'),
+    path('post_create',views.PostCreate.as_view(),name = 'post_create'),
+    # <int:pk>はint型のプライマリキーを示す
+    path('post_detail/<int:pk>',views.PostDetail.as_view(),name='post_detail'),
+    path('post_update/<int:pk>',views.PostUpdate.as_view(),name='post_update'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,resolve_url
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,CreateView,DetailView,UpdateView
 from .models import Post
+from django.urls import reverse_lazy
+from .forms import PostForm
+from django.contrib import messages
 
 
 
@@ -16,3 +19,20 @@ class Index(TemplateView):
     }
     return context
 
+
+class PostCreate(CreateView):
+  model = Post
+  form_class = PostForm
+  success_url = reverse_lazy('myapp:index')
+
+class PostDetail(DetailView):
+  model = Post
+
+
+class PostUpdate(UpdateView):
+  model = Post
+  form_class = PostForm
+
+  def get_success_url(self):
+    messages.info(self.request,'投稿を更新しました。')
+    return resolve_url('myapp:post_detail',pk = self.kwargs['pk'])
